9e534da6d8127bdbf9fd08606fde910f537e6ade
gaoyiwei-wilbur <gaoyiwei_wilbur@163.com>
2015-08-15 17:29:32 +0800

    Initial Commit

---
+++ LeapVisualizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeapVisualizer
+//
+//  Created by Wilbur on 15/8/15.
+//  Copyright (c) 2015年 高意唯. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
